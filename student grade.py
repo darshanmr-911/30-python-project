1f7e27169f41d5ba1dce42364ce5c0bbe87006f0
Darshan M R <143525866+darshanmr-911@users.noreply.github.com>
2026-05-20 21:56:41 +0530

    Add files via upload

---
+++ student grade.py
@@ -0,0 +1,10 @@
+marks=int(input("Enter your marks :"))
+if marks>=90:
+    print("Grade A")
+elif 75<=marks<90:
+    print("Grade B")
+elif 50<=marks<75:
+    print("Grade C")
+else:
+    print("Fail")
+
