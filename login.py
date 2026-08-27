ca776027f17155d9537043260740b256a493fafc
Darshan M R <143525866+darshanmr-911@users.noreply.github.com>
2026-08-27 20:18:21 +0530

    Add files via upload

--- login.py
+++ login.py
@@ -6,8 +6,13 @@ user_password = "xyz37"
 user_name = input("Enter your Username :")
 password = input("Enter your Password :")
 
-if user_name == User_name and password == user_password:
-    print("Login Succuesfuly...!")
+if user_name != User_name:
+    print("Sorry incorrect Username ...!")
 
-else:
-    print("Sorry incorrect")
+
+if user_password != password:
+    print("Sorry incorrect Password ...!")
+    
+elif user_name == User_name and user_password == password:
+    print("Login Succuesfuly...! Welcome to the system")
+
