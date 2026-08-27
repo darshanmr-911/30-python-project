684c95bd6654347d53eeb8730d80c83bfe9d8717
Darshan M R <143525866+darshanmr-911@users.noreply.github.com>
2026-08-27 20:08:21 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,13 @@
+# login interface using python
+
+User_name = "Darshan01"
+user_password = "xyz37"
+
+user_name = input("Enter your Username :")
+password = input("Enter your Password :")
+
+if user_name == User_name and password == user_password:
+    print("Login Succuesfuly...!")
+
+else:
+    print("Sorry incorrect")
